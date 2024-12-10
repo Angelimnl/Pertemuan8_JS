4824b5efacc51cbccd08315f76d252a2b8d95442
Angelimnl <142966170+Angelimnl@users.noreply.github.com>
2024-12-10 15:25:42 +0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,60 @@
+// console.log('Hello World!');
+
+//variables
+// let greeting ='Hello World!';
+// console.log(greeting);
+
+// greeting = 'Halo Dunia';
+// console.log(greeting);
+
+// Examples of data types
+// let name = 'John'; // String
+// let age = 25; // Number
+// let isStudent = true; // Boolean
+// let person = { name: 'John', age: 25 }; // Object
+// let numbers = [1, 2, 3, 4, 5]; // Array
+
+// console.log(person);
+// console.log(numbers);
+
+// // Examples of Arithmec Operators
+// let sum = 10 + 5; // 15
+// let difference = 10 - 5; // 5
+// let product = 10 * 5; // 50
+// let quotient = 10 / 5; // 2
+// let remainder = 10 % 3; // 1
+
+// console.log(remainder);
+
+// Examples of Comparison Operators
+// let isEqual = 5 === '5'; // false (strict equality)
+// let isNotEqual = 10 != '10'; // false
+// let isGreaterThan = 15 > 10; // true
+
+// console.log(isGreaterThan);
+
+// Functions
+// function greet(name) {
+//     let age = 21;
+//     console.log(`Hello, ${name}!`);
+//     }
+//     greet('John'); // Outputs: Hello, John!
+
+// Example of creating an object
+let person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    address: {
+    city: 'New York',
+    country: 'USA'
+    }
+    };
+    
+    person.age = 25;
+    person.job = 'teacher'
+    console.log(person);
+    
+    delete person.age;
+    
+    console.log(person);
